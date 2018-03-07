e83852ff39db9379cef65902cf9b71af5e87088b
Costica Puntaru <Costica.Puntaru@everymatrix.com>
2018-03-07 10:07:23 +0200

    improve

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
+const PORT = 80;
 const log = [];
+const logSize = 50;
 
+app.get('/log', (req, res) => {
+    res.json(log);
+});
+
+app.get('/google0fba1d58ca9ecd9a.html', (req, res) => {
+    res.send('google-site-verification: google0fba1d58ca9ecd9a.html');
+});
 
 app.get('/', (req, res) => {
     log.push(req.headers);
+    if(log.length > logSize){
+        log.shift();
+    }
+
     res.send('Hello World!')
 });
 
-app.get('/log', (req, res) => {
-    res.json(log)
-});
 
-app.listen(80, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
