821bfce4e7439cc9c42643d6467fd5d784a996cc
Costica Puntaru <Costica.Puntaru@everymatrix.com>
2018-03-07 09:47:00 +0200

    fix start script and prot

--- src/index.js
+++ src/index.js
@@ -13,4 +13,4 @@ app.get('/log', (req, res) => {
     res.json(log)
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(80, () => console.log('Example app listening on port 3000!'));
