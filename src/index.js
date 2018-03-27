fefe46525bc992b2c704e71deaffda959f37fa1f
Costica Puntaru <Costica.Puntaru@everymatrix.com>
2018-03-27 09:48:07 +0300

    add custom fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 const log = [];
 const logSize = 50;
 
@@ -13,14 +14,26 @@ app.get('/google0fba1d58ca9ecd9a.html', (req, res) => {
     res.send('google-site-verification: google0fba1d58ca9ecd9a.html');
 });
 
-app.get('/', (req, res) => {
-    log.push(req.headers);
-    if(log.length > logSize){
+function addLog(newLine) {
+    log.push(newLine);
+    if (log.length > logSize) {
         log.shift();
     }
+}
 
-    res.send('Hello World!')
+app.get('/', (req, res) => {
+    addLog(req.headers);
+    res.json({ok: true})
 });
 
+app.post('/add-log', bodyParser.json(), (req, res) => {
+    addLog({
+        headers: req.headers,
+        ts2Response: req.body,
+    });
+    res.json({ok: true})
+});
+
+app.use(express.static('./src/public'));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
